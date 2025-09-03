71a6cc314a5eea044074152747da85a6d213d389
melihsati <melih.sati@etu.unilim.fr>
2025-09-03 09:24:20 +0200

    Deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://melisati.github.io',
+  base: '/BUT3TD',
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
